b89d77fe627ea0f79227805b01527ca22f658648
Abika Chairul Yusri <acyusri@hitachi-ch.co.id>
2024-01-14 20:56:46 +0700

    Add jet coffee skeleton

--- app/src/main/java/com/sinau/mybasiccomposedicoding/MainActivity.kt
+++ app/src/main/java/com/sinau/mybasiccomposedicoding/MainActivity.kt
@@ -3,13 +3,9 @@ package com.sinau.mybasiccomposedicoding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import com.sinau.mybasiccomposedicoding.ui.component.ContactCard
 import com.sinau.mybasiccomposedicoding.ui.theme.MyBasicComposeDicodingTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,22 +14,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyBasicComposeDicodingTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    // Add code here
-                    ContactCard()
-                }
+                JetCoffeApp()
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
 fun ContactCardPreview() {
     MyBasicComposeDicodingTheme {
-        ContactCard()
     }
+}
+
+@Composable
+private fun JetCoffeApp() {
+
 }
